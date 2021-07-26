47bdaca1537600c0ac56ee8ba6f32c45b2d172ed
Valerii <valeravivaz@gmail.com>
2021-07-26 11:31:56 +0300

    add new tasks

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-// first task
+// first task-------------------------------
 
 const myArray = [4, -2, 5, 19, -130, 0, 10];
 
@@ -22,7 +22,7 @@ const returnMinMaxValues = function (myarray){
   return [maxValue, minValue];
 }
 
-// second task
+// second task-----------------------------
 
 const arr = [12, 15, 20, 25, 59, 79];
 
@@ -34,20 +34,20 @@ const returnAverageValue = function(array){
   return (result/array.length);
 }
 
-// third task 
+// third task------------------------------ 
 
 const getRandomNumberFrom = function(){
   return Math.floor(Math.random()*(101));
 }
 
-// fourth task
+// fourth task-----------------------------
 
 let myRandomArray = []; 
 for(let i=0; i < 10; i++){
   myRandomArray[i] = getRandomNumberFrom();
 }
 
-// concat task from document 
+// concat task from document---------------- 
 
 const arr1 = [1, 2, 3];
 const arr2 = [4, 5, 6];
@@ -57,7 +57,7 @@ const arr3 = arr1.concat(arr2);
 
 const arr1Reversed = arr1.reverse();
 
-// push & unshift task from document
+// push & unshift task from document--------
 
 const array1 = [1, 2, 3];
 array1.push(4, 5, 6);
@@ -65,14 +65,47 @@ array1.push(4, 5, 6);
 const array2 = [1, 2, 3];
 array2.unshift(4, 5, 6);
 
-// shift & pop task from document
+// shift & pop task from document-----------
 
 const array4 = ['js', 'css', 'jq'];
 console.log(array4.shift());
 console.log(array4.pop());
 
-// slice task from document 
+// slice task from document-----------------
 
 const array5 = [1, 2, 3, 4, 5];
 const slice1Array5 = array5.slice(0, 3);
 const slice2Array5 = array5.slice(3);
+
+// splice task from document----------------
+
+const array6 = [1, 2, 3, 4, 5];
+array6.splice(1, 2);
+
+//
+
+const array7 = [1, 2, 3, 4, 5];
+const spliceArray1 = array7.splice(1, 3);
+
+//
+
+const array8 = [1, 2, 3, 4, 5];
+array8.splice(3, 0, 'a', 'b', 'c');
+
+//
+
+const array9 = [1, 2, 3, 4, 5];
+array9.splice(1, 0, 'a', 'b');
+array9.splice(6, 0, 'c');
+array9.splice(8, 0, 'e');
+
+// sort task from document-------------------
+
+const array10 = [3, 4, 1, 2, 7];
+array10.sort();
+
+// keys task from document-------------------
+
+const object = {js:'test', jq: 'hello', css: 'world'};
+const arrayObjectKeys = Object.keys(object);
+
